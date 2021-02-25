d84fba0390d2c039c78a9b8d299736b356f52aa7
Yongqin Liu <yongqin.liu@linaro.org>
2021-02-25 20:49:00 +0800

    lcr/settings.py: set STATIC_URL as common
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Change-Id: I64ec3404b38434df5291c80311dfb99f6a063c26

--- lcr/settings.py
+++ lcr/settings.py
@@ -238,11 +238,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_URL = '/static/'
 if DEPLOYED_WITH_APACHE:
     # python  manage.py collectstatic
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 else:
-    STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
