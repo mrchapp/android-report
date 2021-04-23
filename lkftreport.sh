abc55111752ff75e4bcd1adf7be35b1be72b06e6
Yongqin Liu <yongqin.liu@linaro.org>
2021-04-23 22:20:39 +0800

    lkftreport.sh: make lkftreport.sh a generic script
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lkftreport.sh
+++ lkftreport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 dir_parent=$(cd $(dirname $0); pwd)
-
-${dir_parent}/../workspace-python3/bin/python ${dir_parent}/../lcr-report/manage.py lkftreport "$@"
+# need to be run with "sudo -u www-data ./lkftreport.sh" on production environment
+${dir_parent}/../workspace-python3/bin/python ${dir_parent}/manage.py lkftreport "$@"
