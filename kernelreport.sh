f371726f1c310f977f07857294211eb85c78b41b
Yongqin Liu <yongqin.liu@linaro.org>
2021-05-31 19:52:35 +0800

    kernelreport.sh: update to make it more user friendly
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- kernelreport.sh
+++ kernelreport.sh
@@ -1,6 +1,26 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 dir_parent=$(cd $(dirname $0); pwd)
 
-wget https://raw.githubusercontent.com/tom-gall/android-qa-classifier-data/master/flakey.txt -O /tmp/flakey.txt
-${dir_parent}/../workspace-python3/bin/python ${dir_parent}/manage.py kernelreport EAP510 /tmp/kernelreport.txt /tmp/flakey.txt No
+# e.g.
+# ./kernelreport.sh 5.10
+# ./kernelreport.sh 5.10 5.10.40
+ker_version="${1}"
+exact_ker_version=${2:-"No"}
+
+if [ -z "${ker_version}" ]; then
+    echo "Please run like this:"
+    echo -e "\t${0} [4.4|4.9|4.14|4.19|5.4|5.10|EAP510|EAP54]"
+    echo -e "\t${0} [4.4|4.9|4.14|4.19|5.4|5.10|EAP510|EAP54] exact_kernel_version"
+    exit 1
+fi
+if [ "X${exact_ker_version}" = "XNo" ]; then
+    f_report="/tmp/kernelreport-${ker_version}.txt"
+else
+    f_report="/tmp/kernelreport-${exact_ker_version}.txt"
+fi
+
+wget -c https://raw.githubusercontent.com/tom-gall/android-qa-classifier-data/master/flakey.txt -O /tmp/flakey.txt
+${dir_parent}/../workspace-python3/bin/python ${dir_parent}/manage.py kernelreport "${ker_version}" ${f_report} /tmp/flakey.txt "${exact_ker_version}"
+cat ${f_report}.scribble >> ${f_report}
+echo "Please check the file of ${f_report} for report"
